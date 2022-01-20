650183673f5d9a7e75e58b2c5196d0fe7761c233
tornikerogava <tokorogava@gmail.com>
2022-01-20 13:47:56 +0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react"
+import Papa from "papaparse"
+import Table from "./components/table"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface User {
+    id: string
+    first_name: string
+    last_name: string
+    email: string
 }
 
-export default App;
+
+function App() {
+    const [data, setData] = useState<User[]>([])
+    const [columns, setColumns] = useState<any>([])
+
+
+
+    const fetchData = (offset: number, limit: number) => {
+        fetch("/data/MOCK_DATA.csv").then(async (res) => {
+            const reader = res?.body?.getReader()
+            const decoder = new TextDecoder("utf-8")
+
+            const csvData = await reader?.read().then((result) => {
+                return decoder.decode(result.value)
+            })
+
+            csvData &&
+            Papa.parse(csvData, {
+                header: true,
+                complete: (result: any) => {
+                    setColumns(
+                        Object.keys(result.data[0]).map((column) => ({
+                            Header: column.toUpperCase(),
+                            accessor: column,
+                        }))
+                    )
+                    setData((prevState) =>
+                        prevState.concat(result.data.slice(offset, offset + limit))
+                    )
+                },
+            })
+        })
+    }
+
+
+
+    const [paginationData, setPaginationData] = useState({offset: 0, limit: 20});
+
+    useEffect(() => {
+        fetchData(paginationData.offset, paginationData.limit);
+    }, [paginationData])
+
+    const fetchMore = () => {
+        setPaginationData((prevState) => ({
+            ...prevState,
+            offset: prevState.offset + prevState.limit,
+        }))
+    }
+
+    const onEdit = (id: string) => {
+        // Open Modal
+        console.log(id)
+    }
+
+    const onDelete = (id: string) => {
+        setData((prevState) => prevState.filter((profile) => id !== profile.id));
+    }
+
+
+    return (
+        <div className="App">
+            {data[0] ? (
+                <Table
+                    onEdit={onEdit}
+                    onDelete={onDelete}
+                    usersColumns={columns}
+                    usersData={data}
+                />
+            ) : "Loading..."}
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/table.tsx
@@ -0,0 +1,90 @@
+import { useTable } from "react-table"
+
+interface User {
+  id: string
+  first_name: string
+  last_name: string
+  email: string
+}
+
+function Table({
+  usersColumns: columns,
+  usersData: data,
+  onEdit,
+  onDelete,
+}: {
+  usersColumns: any
+  usersData: User[]
+  onEdit: any
+  onDelete: any
+}) {
+  //   const data = useMemo(() => usersData, []);
+  //   const columns = useMemo(() => usersColumns, []);
+  const table = useTable({
+    columns,
+    data,
+  })
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    table
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {
+          // Loop over the header rows
+          headerGroups.map((headerGroup) => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {
+                // Loop over the headers in each row
+                headerGroup.headers.map((column) => (
+                  // Apply the header cell props
+                  <th {...column.getHeaderProps()}>
+                    {
+                      // Render the header
+                      column.render("Header")
+                    }
+                  </th>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </thead>
+      {/* Apply the table body props */}
+      <tbody {...getTableBodyProps()}>
+        {
+          // Loop over the table rows
+          rows.map((row) => {
+            // Prepare the row for display
+            prepareRow(row)
+            return (
+              // Apply the row props
+              <tr {...row.getRowProps()}>
+                {
+                  // Loop over the rows cells
+                  row.cells.map((cell) => {
+                    // Apply the cell props
+                    return (
+                      <td {...cell.getCellProps()}>
+                        {
+                          // Render the cell contents
+                          cell.render("Cell")
+                        }
+                      </td>
+                    )
+                  })
+                }
+                <button onClick={() => onEdit(row.values.id)}>Edit</button>
+                <button onClick={() => onDelete(row.values.id)}>Delete</button>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
